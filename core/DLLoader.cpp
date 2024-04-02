291d517ae26ebeb1a919a17d4f03a6b43c8d37a9
Quentin Tennerel <quentin.tennerel@epitech.eu>
2024-04-02 23:26:45 +0200

    🎨 move the code from the hpp to the cpp

--- core/DLLoader.cpp
+++ core/DLLoader.cpp
@@ -12,6 +12,55 @@
 #include <dlfcn.h>
 #include <memory>
 
+template <typename T>
+Arcade::DLLoader<T>::DLLoader() {
+    handle = nullptr;
+};
+
+template <typename T>
+Arcade::DLLoader<T>::DLLoader(const std::string &entryPoint) : entryPoint(entryPoint) {
+    handle = nullptr;
+}
+
+template <typename T>
+Arcade::DLLoader<T>::~DLLoader() {
+    // if (handle != nullptr)
+    //     dlclose(handle);
+}
+
+template <typename T>
+T *Arcade::DLLoader<T>::getInstance(const std::string &libname) {
+    if (handle != nullptr)
+        dlclose(handle);
+    handle = dlopen(libname.c_str(), RTLD_LAZY);
+    if (!handle) {
+        std::cerr << libname << std::endl;
+        std::cerr << entryPoint << std::endl;
+        std::cerr << "Error1: " << dlerror() << std::endl;
+        exit(84);
+    }
+    T *(*object)(void) = (T*(*)())dlsym(handle, entryPoint.c_str());
+    if (!object) {
+        std::cerr << libname << std::endl;
+        std::cerr << entryPoint << std::endl;
+        std::cerr << "Error2: "  << dlerror() << std::endl;
+        exit(84);
+    }
+    T *tmp = object();
+    if (tmp == nullptr) {
+        std::cerr << libname << std::endl;
+        std::cerr << entryPoint << std::endl;
+        std::cerr << "Error3: " << libname << " " << entryPoint << " returned nullptr" << std::endl;
+        exit(84);
+    }
+    return tmp;
+}
+
+template <typename T>
+void Arcade::DLLoader<T>::setEntryPoint(std::string const &entryPoint) {
+    this->entryPoint = entryPoint;
+}
+
 template class Arcade::DLLoader<int>;
 template class Arcade::DLLoader<Arcade::IGame>;
 template class Arcade::DLLoader<Arcade::IGraphic>;
--- core/DLLoader.hpp
+++ core/DLLoader.hpp
@@ -16,45 +16,11 @@ namespace Arcade {
     template <typename T>
     class DLLoader {
         public :
-            DLLoader() {
-                handle = nullptr;
-            };
-            DLLoader(const std::string &entryPoint) : entryPoint(entryPoint) {
-                handle = nullptr;
-            }
-            ~DLLoader() {
-                // if (handle != nullptr)
-                //     dlclose(handle);
-            }
-            T *getInstance(const std::string &libname) {
-                if (handle != nullptr)
-                    dlclose(handle);
-                handle = dlopen(libname.c_str(), RTLD_LAZY);
-                if (!handle) {
-                    std::cerr << libname << std::endl;
-                    std::cerr << entryPoint << std::endl;
-                    std::cerr << "Error1: " << dlerror() << std::endl;
-                    exit(84);
-                }
-                T *(*object)(void) = (T*(*)())dlsym(handle, entryPoint.c_str());
-                if (!object) {
-                    std::cerr << libname << std::endl;
-                    std::cerr << entryPoint << std::endl;
-                    std::cerr << "Error2: "  << dlerror() << std::endl;
-                    exit(84);
-                }
-                T *tmp = object();
-                if (tmp == nullptr) {
-                    std::cerr << libname << std::endl;
-                    std::cerr << entryPoint << std::endl;
-                    std::cerr << "Error3: " << libname << " " << entryPoint << " returned nullptr" << std::endl;
-                    exit(84);
-                }
-                return tmp;
-            }
-            void setEntryPoint(std::string const &entryPoint) {
-                this->entryPoint = entryPoint;
-            }
+            DLLoader();
+            DLLoader(const std::string &entryPoint);
+            ~DLLoader();
+            T *getInstance(const std::string &libname);
+            void setEntryPoint(std::string const &entryPoint);
         private :
             void *handle;
             std::string entryPoint;
