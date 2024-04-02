1cff428a782d50ac0cab9a904e9a1ad1b57bfedd
6im0n <simon.gl3135@orange.fr>
2024-04-02 13:18:59 +0200

    ✨ Implement displayEntities, displayText, and playSound functions

--- src/sfml/Sfml.cpp
+++ src/sfml/Sfml.cpp
@@ -50,24 +50,46 @@ void Sfml::displayWindow()
 
 void Sfml::displayEntities(std::vector<std::shared_ptr<IEntity>> entities)
 {
-    // this->loadTexture(entities);
-    // for (auto &entity : entities) {
-    //     sf::Sprite sprite;
-    //     sprite.setTexture(this->_textures[0]);
-    //     sprite.setPosition(entity->getPos()[0], entity->getPos()[1]);
-    //     this->_window.draw(sprite);
-    // }
+    this->loadTexture(entities);
+    for (auto &entity : entities) {
+        sf::Sprite sprite;
+        sprite.setTexture(this->_textures[0]);
+        sprite.setPosition(entity->getPos()[0], entity->getPos()[1]);
+        this->_window.draw(sprite);
+    }
     (void)entities;
 }
 
 void Sfml::displayText(std::vector<std::shared_ptr<IText>> texts)
 {
-    (void)texts;
+    for (auto &text : texts) {
+        sf::Text sfmlText;
+        sf::Font font;
+        font.loadFromFile(text->getFontPath());
+        sfmlText.setFont(font);
+        sfmlText.setString(text->getText());
+        sfmlText.setCharacterSize(text->getSize());
+        sfmlText.setFillColor(sf::Color::White);
+        sfmlText.setPosition(text->getPos()[0], text->getPos()[1]);
+        this->_window.draw(sfmlText);
+    }
 }
 
 void Sfml::playSound(std::vector<std::shared_ptr<ISound>> sounds)
 {
-    (void)sounds;
+    for (auto &sound : sounds) {
+        sf::SoundBuffer buffer;
+        if (!buffer.loadFromFile(sound->getPathSound())) {
+            std::cerr << "Error loading sound file" << std::endl;
+            continue;
+        }
+        sf::Sound sfmlSound;
+        sfmlSound.setBuffer(buffer);
+        sfmlSound.play();
+        while (sfmlSound.getStatus() == sf::Sound::Playing) {
+            sf::sleep(sf::milliseconds(100));
+        }
+    }
 }
 
 void Sfml::loadTexture(std::vector<std::shared_ptr<IEntity>> entities)
@@ -79,20 +101,20 @@ void Sfml::loadTexture(std::vector<std::shared_ptr<IEntity>> entities)
     }
 }
 
-void Sfml::loadSound(std::vector<std::shared_ptr<IEntity>> entities)
-{
-    (void)entities;
-}
+// void Sfml::loadSound(std::vector<std::shared_ptr<IEntity>> entities)
+// {
+//     (void)entities;
+// }
 
-void Sfml::loadFont(std::vector<std::shared_ptr<IEntity>> entities)
-{
-    (void)entities;
-}
+// void Sfml::loadFont(std::vector<std::shared_ptr<IEntity>> entities)
+// {
+//     (void)entities;
+// }
 
-void Sfml::loadText(std::shared_ptr<IText> text)
-{
-    (void)text;
-}
+// void Sfml::loadText(std::shared_ptr<IText> text)
+// {
+//     (void)text;
+// }
 
 extern "C" {
     IGraphic *loadGraphicInstance()
