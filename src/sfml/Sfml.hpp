46564f11613811a3dc47556560cac04a0a913efd
6im0n <simon.gl3135@orange.fr>
2024-04-02 13:14:10 +0200

    ✨ Add Sfml.hpp for SFML graphics implementation

---
+++ src/sfml/Sfml.hpp
@@ -0,0 +1,52 @@
+/*
+** EPITECH PROJECT, 2024
+** Arcade_Proj
+** File description:
+** Sfml
+*/
+
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <memory>
+#include "Interfaces/IGraphic.hpp"
+
+class Sfml: public IGraphic {
+    public:
+        Sfml();
+        ~Sfml() override;
+
+        //Window
+        bool isWindowOpen() const override;
+        void closeWindow() override;
+        void clearWindow() override;
+
+        //Event
+        int getKeyEvent() override;
+
+        //Display
+        void displayWindow() override;
+        void displayEntities(std::vector<std::shared_ptr<IEntity>> entities) override;
+        void displayText(std::vector<std::shared_ptr<IText>> texts) override;
+        void playSound(std::vector<std::shared_ptr<ISound>> sounds) override;
+
+    private:
+        sf::RenderWindow _window;
+        sf::Event _event;
+        std::vector<sf::Texture> _textures;
+        std::vector<sf::Sprite> _sprites;
+        std::vector<sf::Text> _texts;
+        std::vector<sf::SoundBuffer> _soundBuffers;
+        std::vector<sf::Sound> _sounds;
+
+        void loadTexture(std::vector<std::shared_ptr<IEntity>> entities);
+        void loadSound(std::vector<std::shared_ptr<IEntity>> entities);
+        void loadFont(std::vector<std::shared_ptr<IEntity>> entities);
+        void loadText(std::shared_ptr<IText> text);
+
+};
